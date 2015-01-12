6fd2d68e44bac760f9181283ece9c20447325190
Michiel Missotten <michiel@lil-web.fr>
2015-01-12 13:33:18 +0100

    Adding a deploy script.

---
+++ L2INFO/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rsync -rv . orange:/srv/www/l2info \
+	--exclude=.git \
+	--exclude=node_modules \
+	--exclude=package.json \
+	--exclude=LICENCE \
+	--exclude=README.md \
+	--exclude=test
