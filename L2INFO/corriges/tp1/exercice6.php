16844f2075406b7795802e236055ef13f9da1c26
Michiel Missotten <michiel@lil-web.fr>
2015-01-20 13:52:35 +0100

    Adding correction for TP1.

---
+++ L2INFO/corriges/tp1/exercice6.php
@@ -0,0 +1,18 @@
+<?php
+
+function getScore($name) {
+    $score = 0;
+    $letters = str_split('abcdefghijklmnopqrstuvwxyz');
+    foreach (str_split(strtolower($name)) as $letter) {
+        if (in_array($letter, $letters)) {
+            $score += array_search($letter, $letters) + 1;
+        }
+    }
+
+    return $score;
+}
+
+echo getScore('PHP') . "\n";
+echo getScore('Lorem ipsum') . "\n";
+echo getScore('Je suis charlie') . "\n";
+echo getScore('we do not forget, we do not forgive, we are legion, expect us.') . "\n";
